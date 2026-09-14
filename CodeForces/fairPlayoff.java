ca3f5d0c69c9d20cff4105b256c3352e4bcad417
Rahul <rathorerahul9050@gmail.com>
2026-09-14 12:05:20 +0530

    Add 1535A problem

---
+++ CodeForces/fairPlayoff.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class fairPlayoff {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            int[] arr = new int[4];
+            for(int j =0;j<4;j++){
+                arr[j] = sc.nextInt();
+            }
+            int w1 = Math.max(arr[0],arr[1]);
+            int l1 = Math.min(arr[0],arr[1]);
+            int w2 = Math.max(arr[2],arr[3]);
+            int l2 = Math.min(arr[2],arr[3]);
+            if(w1<l2 || w2<l1){
+                System.out.println("NO");
+            }
+            else{
+                System.out.println("YES");
+            }
+        }
+        
+    }
+}
