435d7507c2d142db4553f91b734f102447359c5c
Rahul <rathorerahul9050@gmail.com>
2026-09-21 21:46:50 +0530

    Problem B

---
+++ CodeForces/Three_Piles.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Three_Piles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for(int i =0; i<t; i++){
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+
+            long diff = Math.abs(a - b);
+
+            long ans;
+
+            if(a>b){
+                ans = diff + c;
+            } else {
+                ans = Math.max(diff, Math.abs(diff - c));
+            }
+
+            System.out.println(ans);
+        }
+    }
+    
+}
